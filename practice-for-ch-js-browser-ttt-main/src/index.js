a896a875a33da05d773057f9d0a426228c95d70d
Arvid Hossain <arvidh97@gmail.com>
2023-05-02 18:18:48 -0400

    make move

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -6,4 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // Your code here
   const gameNew = new Game();
   const viewNew = new View(gameNew, document.getElementsByClassName("ttt")[0]);
+  const ul = document.querySelector("ul")
+  ul.addEventListener('click', viewNew.handleClick.bind(viewNew));
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -2,27 +2,18 @@ class View {
   constructor(game, el) {
     this.game = game;
     this.el = el;
+    this.setupBoard();
+
+    //this.handleClick = this.handleClick.bind(this)
+    //document.removeEventListener('click', this.handleClick)
   }
   
   setupBoard() {
-    // const board = [];
-    // for (let i = 0; i < 3; i++) {
-    //   board.push([[], [], []])
-    // }
-    // board.forEach((ele) => {
-    //   let li = document.createElement("li");
-      
-    // })
-    // this.game.board.forEach((ele) => {
-    //   ele.forEach((pos) => {
-    //     let li = document.createElement("li")  
-    //   })
-    // })
     const ul = document.createElement("ul");
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         let li = document.createElement("li");
-        li.attr = "pos"
+        li.dataset.pos = JSON.stringify([i,j]);
         ul.appendChild(li);
       }
     }
@@ -30,6 +21,13 @@ class View {
   }
   
   handleClick(e) {
+    // e.preventDefault();
+    const cell = e.target;
+    const parseCell = JSON.parse(cell.dataset.pos);
+    const player = this.game.currentPlayer
+    this.game.playMove(parseCell);
+    cell.innerText = player;
+
   }
 
   makeMove(square) {
